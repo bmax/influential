8929e404e1dda5cd0e246e6603f9310eba5d5346
Brandon Max <bmax1337@gmail.com>
2017-03-11 17:02:35 -0500

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
+  },
+  devServer: { contentBase: path.join(__dirname, "dist"), inline: true }
+};
